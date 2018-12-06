bb9344be328412fbba1c6f4b1dd698551189fa8e
yes-i-am-mr-robot <42581396+yes-i-am-mr-robot@users.noreply.github.com>
2018-12-06 14:59:59 +0530

    Update NaiveBayes.py

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,133 +1,65 @@
-print("\nNaive Bayes Classifier for concept learning problem")
 import csv
-import random
 import math
-import operator
-def safe_div(x,y):
-    if y == 0:
-        return 0
-    return x / y
-	
-def loadCsv(filename):
-	lines = csv.reader(open(filename))
-	dataset = list(lines)
-	for i in range(len(dataset)):
-		dataset[i] = [float(x) for x in dataset[i]]
-	return dataset
- 
-def splitDataset(dataset, splitRatio):
-	trainSize = int(len(dataset) * splitRatio)
-	trainSet = []
-	copy = list(dataset)
-	i=0
-	while len(trainSet) < trainSize:
-		#index = random.randrange(len(copy))
-		
-		trainSet.append(copy.pop(i))
-	return [trainSet, copy]
- 
-def separateByClass(dataset):
-	separated = {}
-	for i in range(len(dataset)):
-		vector = dataset[i]
-		if (vector[-1] not in separated):
-			separated[vector[-1]] = []
-		separated[vector[-1]].append(vector)
-		#print(separated[vector[-1]])
-	return separated
- 
 def mean(numbers):
-	return safe_div(sum(numbers),float(len(numbers)))
-	
+	return sum(numbers)/float(len(numbers))
 def stdev(numbers):
-	avg = mean(numbers)		
-	variance = safe_div(sum([pow(x-avg,2) for x in numbers]),float(len(numbers)-1))
+	avg = mean(numbers)
+	variance = sum([pow(x-avg, 2) for x in numbers])/float(len(numbers)-1)
 	return math.sqrt(variance)
- 
 def summarize(dataset):
 	summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
-	#print("mean and stddev",summaries)
 	del summaries[-1]
 	return summaries
-    
-def summarizeByClass(dataset):
-	separated = separateByClass(dataset)
-	summaries = {}	
-	for classValue, instances in separated.items():		
-		summaries[classValue] = summarize(instances)
-                
-	return summaries
- 
-def calculateProbability(x, mean, stdev):	
-	exponent = math.exp(-safe_div(math.pow(x-mean,2),(2*math.pow(stdev,2))))
-	final = safe_div(1 , (math.sqrt(2*math.pi) * stdev)) * exponent
-	return final
- 
-def calculateClassProbabilities(summaries, inputVector):
-	probabilities = {}
-	for classValue, classSummaries in summaries.items():
-		probabilities[classValue] = 1
-		for i in range(len(classSummaries)):
-			mean, stdev = classSummaries[i]			
-			x = inputVector[i]
-			probabilities[classValue] *= calculateProbability(x, mean, stdev)
-	return probabilities
-			
-def predict(summaries, inputVector):
-	probabilities = calculateClassProbabilities(summaries, inputVector)
-	bestLabel, bestProb = None, -1
-	for classValue, probability in probabilities.items():
-		if bestLabel is None or probability > bestProb:
-			bestProb = probability
-			bestLabel = classValue
-	return bestLabel
- 
-def getPredictions(summaries, testSet):
-	predictions = []
-	for i in range(len(testSet)):
-		result = predict(summaries, testSet[i])
-		predictions.append(result)
-	return predictions
- 
-def getAccuracy(testSet, predictions):
-	correct = 0
-	for i in range(len(testSet)):
-		if testSet[i][-1] == predictions[i]:
-			correct += 1
-	accuracy = safe_div(correct,float(len(testSet))) * 100.0
-	return accuracy
- 
-def main():
-	filename = 'ConceptLearning.csv'
-	splitRatio = 0.75
-	dataset = loadCsv(filename)
-	trainingSet, testSet = splitDataset(dataset, splitRatio)
-	print('Split {0} rows into'.format(len(dataset)))
-	print('Number of Training data: ' + (repr(len(trainingSet))))
-	print('Number of Test Data: ' + (repr(len(testSet))))
-	print("\nThe values assumed for the concept learning attributes are\n")
-	print("OUTLOOK=> Sunny=1 Overcast=2 Rain=3\nTEMPERATURE=> Hot=1 Mild=2 Cool=3\nHUMIDITY=> High=1 Normal=2\nWIND=> Weak=1 Strong=2")
-	print("TARGET CONCEPT:PLAY TENNIS=> Yes=10 No=5")
-	print("\nThe Training set are:")
-	for x in trainingSet:
-		print(x)
-	print("\nThe Test data set are:")
-	for x in testSet:
-		print(x)
-	print("\n")
-	# prepare model
-	summaries = summarizeByClass(trainingSet)
-	# test model
-	predictions = getPredictions(summaries, testSet)
-	actual = []
-	for i in range(len(testSet)):
-		vector = testSet[i]
-		actual.append(vector[-1])
-	# Since there are five attribute values, each attribute constitutes to 20% accuracy. So if all attributes match with predictions then 100% accuracy
-	print('Actual values: {0}%'.format(actual))	
-	print('Predictions: {0}%'.format(predictions))
-	accuracy = getAccuracy(testSet, predictions)
-	print('Accuracy: {0}%'.format(accuracy))
- 
-main()
-
+def calcProb(summary, item):
+	prob = 1
+	for i in range(len(summary)):
+		x = item[i]
+		mean, stdev = summary[i]
+		exponent = math.exp(-math.pow(x-mean, 2) / (2*math.pow(stdev, 2)))
+		final = exponent / (math.sqrt(2*math.pi) * stdev)
+		prob *= final
+	return prob
+with open('ConceptLearning.csv') as csvFile:
+	data = [line for line in csv.reader(csvFile)]
+for i in range(len(data)):
+	data[i] = [float(x) for x in data[i]]
+split = int(0.90 * len(data)) 
+train = []
+test = []
+train = data[:split]
+test = data[split:] 
+print("{} input rows is split into {} training and {} testing datasets".format(len(data), len(train), len(test)))
+print("\nThe values assumed for the concept learning attributes are\n")
+print("OUTLOOK=> Sunny=1 Overcast=2 Rain=3\nTEMPERATURE=> Hot=1 Mild=2 Cool=3\nHUMIDITY=> High=1 Normal=2\nWIND=> Weak=1 Strong=2")
+print("TARGET CONCEPT:PLAY TENNIS=> Yes=10 No=5")
+print("\nThe Training set are:")
+for x in train:
+	print(x)
+print("\nThe Test data set are:")
+for x in test:
+	print(x)
+yes = []
+no = []
+for i in range(len(train)):
+	if data[i][-1] == 5.0:
+		no.append(data[i])
+	else:
+		yes.append(data[i])
+yes = summarize(yes)
+no = summarize(no)
+predictions = [] 
+for item in test:
+	yesProb = calcProb(yes, item)
+	noProb = calcProb(no, item)
+	predictions.append(10.0 if(yesProb > noProb) else 5.0)
+correct = 0
+for i in range(len(test)):
+	if(test[i][-1] == predictions[i]):
+		correct += 1
+print("\nActual values are:")
+for i in range(len(test)):
+	print(test[i][-1], end=" ")
+print("\nPredicted values are:")
+for i in range(len(predictions)):
+	print(predictions[i], end=" ")
+print("\nAccuracy is {}%".format(float(correct/len(test) * 100)))
